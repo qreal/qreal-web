ce094a11ffb393114a0db69abfff5e6647abf4c9
Dmitriy Belokurov <belokurovdmitriy@gmail.com>
2013-03-02 22:15:39 +0400

    Widget superclass

--- MobileCreator/MobileCreator/src/designer/model/Widget.ts
+++ MobileCreator/MobileCreator/src/designer/model/Widget.ts
@@ -3,13 +3,26 @@
 export class Widget {
     private layoutWidth;
     private layoutHeight;
-    private control;
-    private controlType;
+
 
     constructor(layoutWidth, layoutHeight, control: JQuery, controlType) {
         this.layoutWidth = layoutWidth;
         this.layoutHeight = layoutHeight;
-        this.control = control;
-        this.controlType = controlType;
+    }
+
+    get LayoutWidth() {
+        return this.layoutWidth;
+    }
+
+    set LayoutWidth(layoutWidth) {
+        this.layoutWidth = LayoutWidth;
+    }
+
+    get LayoutHeight() {
+        return this.layoutWidth;
+    }
+
+    set LayoutHeight(layoutHeight) {
+        this.layoutWidth = LayoutHeight;
     }
 }
