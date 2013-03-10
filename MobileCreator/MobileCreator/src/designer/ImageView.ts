89aed1cfcc4c9a76167c6388d735e9e3393d8c48
Dmitriy Belokurov <belokurovdmitriy@gmail.com>
2013-03-10 18:53:31 +0400

    ImageView XML export

---
+++ MobileCreator/MobileCreator/src/designer/ImageView.ts
@@ -0,0 +1,56 @@
+/// <reference path="../../lib/jquery.d.ts" />
+/// <reference path="../../lib/jquerymobile.d.ts" />
+import mElement = module("designer/Element");
+import mElementPreferences = module("designer/ElementPreferences")
+import mImageViewPreferences = module("designer/ImageViewPreferences")
+
+export class ImageView extends mElement.Element {
+    private preferences: mImageViewPreferences.ImageViewPreferences;
+    get Preferences() {
+        return this.preferences;
+    }
+    set Preferences(preferences: mImageViewPreferences.ImageViewPreferences) {
+        this.preferences = preferences;
+    }
+    constructor(preferences: mImageViewPreferences.ImageViewPreferences);
+    constructor(preferences: mImageViewPreferences.ImageViewPreferences, domElement: JQuery);
+    constructor(preferences: mImageViewPreferences.ImageViewPreferences, domElement?: JQuery = $("<div></div>")) {
+        super(domElement);
+        this.Preferences = preferences;
+        var image = $("<img></img>");
+        image.attr('src', this.preferences.ImageURL);
+        this.DomElement.append(image);
+        this.applyHeight();
+        this.applyWidth();
+    }
+    public init() {
+        this.DomElement.empty();
+        var image = $("<img></img>");
+        image.attr('src', this.preferences.ImageURL);
+        this.DomElement.append(image);
+        this.applyHeight();
+        this.applyWidth();
+    }
+    public toXML() {
+        var xmlString = "";
+        xmlString += "<ImageView \n";
+        if (this.preferences.Width == mElementPreferences.ElementPreferences.FillParent) {
+            xmlString += "layout_width=\"fill_parent\" ";
+        } else if (this.preferences.Width == mElementPreferences.ElementPreferences.WrapContent) {
+            xmlString += "layout_width=\"wrap_content\" ";
+        } else {
+            xmlString += "layout_width=\"" + this.preferences.Width + "px\" ";
+        }
+        if (this.preferences.Height == mElementPreferences.ElementPreferences.FillParent) {
+            xmlString += "layout_height=\"fill_parent\" ";
+        } else if (this.preferences.Height == mElementPreferences.ElementPreferences.WrapContent) {
+            xmlString += "layout_height=\"wrap_content\" ";
+        } else {
+            xmlString += "layout_height=\"" + this.preferences.Width + "px\" ";
+        }
+        xmlString += "src=\"" + this.preferences.Src + "\" ";
+        xmlString += "layout_gravity=\"" + this.preferences.LayoutGravity + "\" ";
+        xmlString += "layout_marginTop=\"" + this.preferences.LayoutMarginTop + "px\" ";
+        xmlString += "</ImageView>\n"
+    }
+}
