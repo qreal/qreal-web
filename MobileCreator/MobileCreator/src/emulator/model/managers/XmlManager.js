f1564159fad9a09e4b41a528f2b2663d3820d526
NikitaBumakov <nikita.bumakov@gmail.com>
2013-03-11 15:20:39 +0400

    xml parsing

--- MobileCreator/MobileCreator/src/emulator/model/managers/XmlManager.js
+++ MobileCreator/MobileCreator/src/emulator/model/managers/XmlManager.js
@@ -1,7 +1,14 @@
-define(["require", "exports", "utils/log/Log"], function(require, exports, __mLog__) {
+define(["require", "exports", "utils/log/Log", "emulator/model/ui/LinearLayout", "emulator/model/attributes/LinearLayoutTag", "emulator/model/attributes/ControlTag"], function(require, exports, __mLog__, __mLinearLayout__, __mLinearLayoutTag__, __mControlTag__) {
     var mLog = __mLog__;
 
     
+    
+    var mLinearLayout = __mLinearLayout__;
+
+    var mLinearLayoutTag = __mLinearLayoutTag__;
+
+    var mControlTag = __mControlTag__;
+
     var XmlManager = (function () {
         function XmlManager() {
             this.logger = new mLog.Logger("XmlManager");
@@ -9,6 +16,7 @@ define(["require", "exports", "utils/log/Log"], function(require, exports, __mLo
         }
         XmlManager.LinearLayout = "LinearLayout";
         XmlManager.prototype.parsePage = function () {
+            this.logger.log("parse page");
             var xmlHTTP = new XMLHttpRequest();
             var xmlDoc;
             try  {
@@ -19,7 +27,41 @@ define(["require", "exports", "utils/log/Log"], function(require, exports, __mLo
                 window.alert("Unable to load the requested file.");
                 return;
             }
-            this.logger.log(xmlDoc.firstChild.nodeName);
+            this.parseNode(xmlDoc.firstChild);
+        };
+        XmlManager.prototype.parseNode = function (node) {
+            this.logger.log("parse node: " + node.nodeName);
+            switch(node.nodeName) {
+                case XmlManager.LinearLayout:
+                    this.parseLinearLayout(node);
+                    break;
+            }
+        };
+        XmlManager.prototype.parseLinearLayout = function (node) {
+            var tag = new mLinearLayoutTag.LinearLayoutTag();
+            var orientation = node.attributes['orientation'];
+            if(orientation) {
+                tag.Orientation = orientation == 'vertical' ? mLinearLayoutTag.LinearLayoutTag.Vertical : mLinearLayoutTag.LinearLayoutTag.Horizontal;
+            } else {
+                tag.Orientation = mLinearLayoutTag.LinearLayoutTag.Vertical;
+            }
+            var layout_width = node.attributes['layout_width'];
+            if(layout_width) {
+                tag.Width = layout_width == 'wrap_content' ? mControlTag.ControlTag.WrapContent : mControlTag.ControlTag.MatchParrent;
+            } else {
+                tag.Width = mControlTag.ControlTag.MatchParrent;
+            }
+            var layout_height = node.attributes['layout_height'];
+            if(layout_height) {
+                tag.Height = layout_height == 'wrap_content' ? mControlTag.ControlTag.WrapContent : mControlTag.ControlTag.MatchParrent;
+            } else {
+                tag.Height = mControlTag.ControlTag.MatchParrent;
+            }
+            var background = node.attributes['background'];
+            if(background) {
+                tag.Background = background;
+            }
+            return new mLinearLayout.LinearLayout(tag);
         };
         return XmlManager;
     })();
--- MobileCreator/MobileCreator/src/emulator/model/managers/XmlManager.ts
+++ MobileCreator/MobileCreator/src/emulator/model/managers/XmlManager.ts
@@ -1,18 +1,25 @@
 import mLog = module("utils/log/Log");
 import mControl = module("emulator/model/ui/Control");
+import mControlPanel = module("emulator/model/ui/ControlPanel");
+import mLinearLayout = module("emulator/model/ui/LinearLayout");
+import mLinearLayoutTag = module("emulator/model/attributes/LinearLayoutTag");
+import mControlTag = module("emulator/model/attributes/ControlTag");
 
 export class XmlManager {
     private logger = new mLog.Logger("XmlManager");
 
     private static LinearLayout = "LinearLayout";
 
+    private root: mControlPanel.ControlPanel;
+
     constructor() {
         this.logger.log("in constructor");
     }
 
     public parsePage() {
+        this.logger.log("parse page");
         var xmlHTTP = new XMLHttpRequest();
-        var xmlDoc:Document;
+        var xmlDoc: Document;
         try {
             xmlHTTP.open("GET", "res/main.xml", false);
             xmlHTTP.send(null);
@@ -23,8 +30,55 @@ export class XmlManager {
             return;
         }
 
-        this.logger.log(xmlDoc.firstChild.nodeName);
+        this.parseNode(xmlDoc.firstChild);
+    }
 
+    private parseNode(node: Node) {
+        this.logger.log("parse node: " + node.nodeName);
+        switch (node.nodeName) {
+            case XmlManager.LinearLayout:
+                this.parseLinearLayout(node);
+                break;
+        }
+    }
+
+
+    private parseLinearLayout(node: Node) {
+        var tag = new mLinearLayoutTag.LinearLayoutTag();
+
+        var orientation = node.attributes['orientation'];
+        if (orientation) {
+            tag.Orientation = orientation == 'vertical' ?
+                mLinearLayoutTag.LinearLayoutTag.Vertical :
+                mLinearLayoutTag.LinearLayoutTag.Horizontal
+        } else {
+            tag.Orientation = mLinearLayoutTag.LinearLayoutTag.Vertical;
+        }
+
+        var layout_width = node.attributes['layout_width'];
+        if (layout_width) {
+            tag.Width = layout_width == 'wrap_content' ?
+                mControlTag.ControlTag.WrapContent :
+                mControlTag.ControlTag.MatchParrent;
+        } else {
+            tag.Width = mControlTag.ControlTag.MatchParrent;
+        }
+
+        var layout_height = node.attributes['layout_height'];
+        if (layout_height) {
+            tag.Height = layout_height == 'wrap_content' ?
+                mControlTag.ControlTag.WrapContent :
+                mControlTag.ControlTag.MatchParrent;
+        } else {
+            tag.Height = mControlTag.ControlTag.MatchParrent;
+        }
+
+        var background = node.attributes['background'];
+        if (background) {
+            tag.Background = background;
+        }
+
+        return new mLinearLayout.LinearLayout(tag);
     }
 
 }
