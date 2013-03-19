a6cdf3e461aed3281552039f6315a1c2670c548f
Dmitriy Belokurov <belokurovdmitriy@gmail.com>
2013-03-19 17:10:47 +0400

    AppConfig.js bug fix part 1

--- MobileCreator/MobileCreator/src/appConfig.js
+++
@@ -1,8 +0,0 @@
-define(["require", "exports", "./Application"], function(require, exports, __main__) {
-    var main = __main__;
-
-    require([], function () {
-        var appMain = new main.Application();
-        appMain.onCreate();
-    });
-})
