c75225f168cde77a6afefbf31c323837285d2540
Dmitriy Belokurov <belokurovdmitriy@gmail.com>
2013-03-25 17:11:13 +0400

    Form trigger xml export

--- MobileCreator/MobileCreator/src/designer/logic/FormTrigger.ts
+++ MobileCreator/MobileCreator/src/designer/logic/FormTrigger.ts
@@ -3,9 +3,9 @@ import mAction = module("designer/logic/Action")
 export class FormTrigger {
     private formId: string;
     private triggerName: string;
-    private action: mAction.Action;
+    private actions: mAction.Action[];
     constructor(formId: string, triggerName: string) {
-        //this.action = 
+        this.actions = [];
         this.formId = formId;
         this.triggerName = triggerName;
     }
@@ -21,15 +21,47 @@ export class FormTrigger {
     set TriggerName(triggerName: string) {
         this.triggerName = triggerName;
     }
-    get Action() {
-        return this.action;
+    get Actions() {
+        return this.actions;
     }
-    set Action(action: mAction.Action) {
-        this.action = action;
+    set Actions(actions: mAction.Action[]) {
+        this.actions = actions;
+    }
+    public addAction(action: mAction.Action) {
+        this.actions.push(action);
     }
     public toXML() {
         var xml = "<trigger form-id='" + this.formId + "' event='" + this.triggerName + "'>\n";
-        xml += this.action.toXML();
+        var seqCount = 0;
+        for (var i = 0; i < this.actions.length - 1; i++) {
+            if (this.actions.length - 1 == 1) {
+                xml += this.actions[i].toXML();
+                break;
+            }
+            if (this.actions.length - i - 2 == 1) {
+                xml += "<seq>\n";
+                xml += "<first-operator>\n";
+                xml += this.actions[i].toXML();
+                xml += "</first-operator>\n";
+                xml += "<second-operator>\n";
+                xml += this.actions[i + 1].toXML();
+                xml += "</second-operator>\n";
+                xml += "</seq>\n";
+                break;
+            }
+            else {
+                xml += "<seq>\n";
+                xml += "<first-operator>\n";
+                xml += this.actions[i].toXML();
+                xml += "</first-operator>\n";
+                xml += "<second-operator>\n";
+                seqCount++;
+            }
+        }
+        for (var i = 0; i < seqCount; i++) {
+            xml += "</second-operator>\n";
+            xml += "</seq>\n";
+        }
         xml += "</trigger>\n"
         return xml;
     }
