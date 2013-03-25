368f5863465d748015fbcc8e401669881013e253
Nadya <nadya.chizhova@gmail.com>
2013-03-25 21:33:34 +0400

    Login-Password

---
+++ AppServer/Sender/Sender/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sender
+{
+    public class Login
+    {
+        public string login;
+        public string password;
+        public Login() { }
+        public Login(string login, string password) { this.login = login; this.password = password; }
+    }
+    class Program
+    {
+        public static string HttpPost(string URI, string Parameters)
+        {
+            System.Net.WebRequest req = System.Net.WebRequest.Create(URI);
+            //req.Proxy = new System.Net.WebProxy(ProxyString, true);
+            //Add these, as we're doing a POST
+            req.ContentType = "application/x-www-form-urlencoded";
+            req.Method = "POST";
+            //We need to count how many bytes we're sending. 
+            //Post'ed Faked Forms should be name=value&
+            byte[] bytes = System.Text.Encoding.ASCII.GetBytes(Parameters);
+            req.ContentLength = bytes.Length;
+            System.IO.Stream os = req.GetRequestStream();
+            os.Write(bytes, 0, bytes.Length); //Push it out there
+            os.Close();
+            System.Net.WebResponse resp = req.GetResponse();
+            if (resp == null) return null;
+            return new System.IO.StreamReader(resp.GetResponseStream()).ReadToEnd().Trim();
+        }
+        static void Main(string[] args)
+        {
+            var a = HttpPost("http://localhost:54321/", "Login:" + new System.Web.Script.Serialization.JavaScriptSerializer().Serialize(new Login("Chizh", "passwor")));
+            Console.WriteLine(a);
+        }
+    }
+}
---
+++ AppServer/Server/Server/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Specialized;
+using System.IO;
+using System.Net;
+using System.Reflection;
+using System.Text;
+using System.Threading;
+
+namespace Server {
+    public class Login {
+        public string login;
+        public string password;
+        public Login() { }
+        public Login(string login, string password) { this.login = login; this.password = password; }
+    }
+
+    class Program {
+        static void Main() {
+            HttpListener listener = new HttpListener();
+            listener.Prefixes.Add("http://localhost:54321/");
+            listener.Start();
+            Console.WriteLine("Listening...");
+            for (; ; ) {
+                HttpListenerContext ctx = listener.GetContext();
+                new Thread(new Worker(ctx).ProcessRequest).Start();
+            }
+        }
+
+        class Worker {
+            private HttpListenerContext context;
+
+            public Worker(HttpListenerContext context) {
+                this.context = context;
+            }
+
+            public void ProcessRequest() {
+                string input = new StreamReader(context.Request.InputStream).ReadToEnd();
+                string msg = context.Request.HttpMethod + " " + context.Request.Url;
+                Console.WriteLine(msg);
+
+                StringBuilder sb = new StringBuilder();
+
+                var delimiter = input.IndexOf(":");
+                var name = input.Substring(0, delimiter);
+                var data = input.Substring(delimiter + 1);
+                Console.WriteLine(data);
+
+                if (name.ToLower() == "login") {
+                    var login = new System.Web.Script.Serialization.JavaScriptSerializer().Deserialize<Login>(data);
+                    if (login.login == "Chizh" && login.password == "password") {
+                        sb.Append("success:1");
+                    }
+                    else {
+                        sb.Append("fail");
+                    }
+                }
+
+                byte[] b = Encoding.UTF8.GetBytes(sb.ToString());
+                context.Response.ContentLength64 = b.Length;
+                context.Response.OutputStream.Write(b, 0, b.Length);
+                context.Response.OutputStream.Close();
+            }
+        }
+    }
+}
