ec69411d27205984406fd3c824b5a124e654912f
NikitaBumakov <nikita.bumakov@gmail.com>
2013-03-26 13:41:42 +0400

    Integrated Mobile creator with Server

---
+++ MobileCreator/QRealWeb/Website/Canvas.fs
@@ -0,0 +1,110 @@
+﻿module Website.Canvas
+
+open IntelliFactory.WebSharper
+open IntelliFactory.WebSharper.Html
+open IntelliFactory.WebSharper.Raphael
+open IntelliFactory.WebSharper.JQuery
+
+module SampleInternals =
+    open Primitives
+    [<JavaScript>]
+    let SimpleExample () =
+        Div []
+        |>! OnAfterRender (fun elem -> 
+            let paper = Raphael(elem.Body, 400., 250.)
+            let c = paper.Circle(90., 90., 70.)
+            paper.Text(50., 50., "Hello\nRaphael") |> ignore
+            paper.Path("M10 10L90 90") |> ignore
+            paper.Ellipse(250., 50., 40., 20.) |> ignore
+            paper.Rect(250., 140., 50., 50., 10.) |> ignore
+            c.Fill("#d0d") |> ignore)
+
+    [<JavaScript>]
+    let Main () = 
+        Div []
+        |>! OnAfterRender (fun elem -> 
+            let leftWidth = 150.0
+            let width = 300.0
+            let height = 500.0
+            let canvas = new Raphael(elem.Body, leftWidth + width, height)
+            let startX, startY = leftWidth, 0.0
+            canvas.Rect(0.0, 0.0, leftWidth + width, height) |> ignore
+            let line x1 y1 x2 y2 =
+                let path = new Path()
+                canvas.Path(path.MoveTo(x1, y1).LineTo(x2-x1, y2-y1).ClosePath()) |> ignore
+            //canvas.Rect(startX - leftWidth, startY, leftWidth, height) |> ignore
+            line leftWidth 0.0 leftWidth height
+            let genBtn =
+                let num = ref 0
+                fun () ->
+                    incr num
+                    let btn = new Button("button", Value 100.0, Value 50.0, 50.0, 25.0 - leftWidth, "someText", 20.0, "method")
+                    (btn :> Primitive).draw canvas leftWidth 0.0
+                    btn
+            let (tools : VisiblePrimitive list ref) = ref [genBtn()]
+            
+            let existing = ref []
+                //genBtn()
+            //let rect = canvas.Rect(50.0, 50.0, 100.0, 50.0)
+            //rect.Translate(300.0, 100.0)
+            //(btn :> Primitive).draw canvas leftWidth 0.0
+            
+            (*let text = "Hello World"
+            let txt = 
+                [| "120deg"
+                   "0"
+                   "240deg" |]
+                |> Array.map (fun fill ->
+                    canvas.Text(150., 160., text)
+                        .Font("50px Helvetica, Arial")
+                        .Opacity(0.5)
+                        .Fill("hsb(" + fill + ", .5, 1)"))*)
+            //let mouse = ref None
+            let rot = ref 0.
+            let prevX, prevY = ref 0, ref 0
+            let topX, topY = canvas.Top.X(), canvas.Top.Y()
+            let selected = ref None
+            let isTool = ref false
+            //JQuery.JQuery.Of(Dom.Document.Current).Mousedown(fun _ e ->
+            let curh = ref 200.0
+            JQuery.JQuery.Of(Dom.Document.Current).Mousedown(fun _ e ->
+                let findElems = List.tryFind (fun (elem : VisiblePrimitive) ->
+                    elem.IsInside (float e.PageX - leftWidth - 216.0) (float e.PageY - 152.0))
+                match findElems !tools with
+                | Some elem ->
+                    selected := Some elem
+                    prevX := e.PageX
+                    prevY := e.PageY
+                    isTool := true
+                | None ->
+                    match findElems !existing with
+                    | Some elem ->
+                        selected := Some elem
+                        isTool := false
+                    | None -> ()
+                curh := !curh + 20.0
+                canvas.Safari()
+            ) |> ignore
+            JQuery.JQuery.Of(Dom.Document.Current).Mousemove(fun _ e -> 
+                match !selected with
+                | None -> ()
+                | Some elem -> 
+                    elem.Move (float (e.PageX - !prevX)) (float (e.PageY - !prevY))
+                    prevX := e.PageX
+                    prevY := e.PageY
+                    canvas.Safari()
+            ) |> ignore
+            JQuery.JQuery.Of(Dom.Document.Current).Mouseup(fun _ e -> 
+                match !selected with
+                | None -> ()
+                | Some elem -> 
+                    selected := None
+            ) |> ignore
+            )
+
+
+type RaphaelViewer() =
+    inherit Web.Control()
+
+    [<JavaScript>]
+    override this.Body = SampleInternals.Main() :> IPagelet
---
+++ MobileCreator/QRealWeb/Website/Forms.fs
@@ -0,0 +1,253 @@
+﻿namespace Website
+
+open IntelliFactory.WebSharper
+open IntelliFactory.WebSharper.Formlet
+open IntelliFactory.WebSharper.Html
+open IntelliFactory.WebSharper.Sitelets
+open IntelliFactory.WebSharper.Web
+
+module DB = 
+    open System.Data.SqlClient
+
+    let sqlConn = "Data Source=(local);Initial Catalog=Movies;Integrated Security=True"
+
+    let moviesDB = new SqlConnection(sqlConn)
+    moviesDB.Open()
+    [<Rpc>]
+    let selectAll =
+        let sql = "SELECT * FROM Movie"
+        let cmd = new SqlCommand(sql, moviesDB)
+        fun () ->
+            let res = new ResizeArray<_>()
+            let reader = cmd.ExecuteReader()
+            while reader.Read() do
+                res.Add <| Array.init (reader.FieldCount) (fun i -> reader.[i])
+            reader.Close()
+            res.ToArray()
+
+    [<Rpc>]
+    let addValue value =
+        let sql = "Insert into Movie values ('" + value + "')"
+        let cmd = new SqlCommand(sql, moviesDB)
+        cmd.ExecuteNonQuery()
+
+    [<Rpc>]
+    let checkLogin (login : string) (password : string) =
+        let sql = "Select CASE WHEN EXISTS (SELECT * FROM [User] us WHERE us.Login = @login and us.Password = @password) THEN 1 ELSE 0 END"
+        let cmd = new SqlCommand(sql, moviesDB)
+        cmd.Parameters.AddWithValue("@login", login) |> ignore
+        cmd.Parameters.AddWithValue("@password", password) |> ignore
+        unbox<int> (cmd.ExecuteScalar()) <> 0
+        
+    [<Rpc>]
+    let existsUser (login : string) =
+        let sql = "Select CASE WHEN EXISTS (SELECT * FROM [User] us WHERE us.Login = @login) THEN 1 ELSE 0 END"
+        let cmd = new SqlCommand(sql, moviesDB)
+        cmd.Parameters.AddWithValue("@login", login) |> ignore
+        unbox<int> (cmd.ExecuteScalar()) <> 0
+        
+    [<Rpc>]
+    let addUser (login : string) (password : string) =
+        let sql = "insert into [User] values (@login, @password)"
+        let cmd = new SqlCommand(sql, moviesDB)
+        cmd.Parameters.AddWithValue("@login", login) |> ignore
+        cmd.Parameters.AddWithValue("@password", password) |> ignore
+        cmd.ExecuteNonQuery()
+        
+
+/// This module defines the client-side functionality used by the website.
+module Forms =
+
+    type LoginInfo =
+        {
+            Name : string
+            Password : string
+        }
+
+    [<JavaScript>]
+    let Input (label: string) (err: string) =
+        Controls.Input ""
+        |> Validator.IsNotEmpty err
+        |> Enhance.WithValidationIcon
+        |> Enhance.WithTextLabel label
+
+    [<JavaScript>]
+    let InputInt (label: string) (err: string) =
+        Controls.Input ""
+        |> Validator.IsInt err
+        |> Enhance.WithValidationIcon
+        |> Enhance.WithTextLabel label
+        |> Formlet.Map int
+
+    [<JavaScript>]
+    let MovieTitleForm () : Formlet<string> =
+        Formlet.Yield (fun name-> name)
+        <*> Input "Title" "Please enter a title"
+
+    [<JavaScript>]
+    let AddMovie () =
+        let textForm =
+            MovieTitleForm ()
+            |> Enhance.WithSubmitButton
+            |> Enhance.WithCustomFormContainer {
+                 Enhance.FormContainerConfiguration.Default with
+                    Header =
+                        "Type Movie title to add"
+                        |> Enhance.FormPart.Text
+                        |> Some
+                    Description =
+                        "Please enter movie title."
+                        |> Enhance.FormPart.Text
+                        |> Some
+               }
+        let proc (title : string) () =
+            ignore <| DB.addValue title
+            FieldSet [
+                P ["Title is succesfully added" |> Text]
+            ]
+
+        Formlet.Do {
+            let! t = textForm
+            return! Formlet.OfElement (proc t)
+        }
+        |> Formlet.Flowlet
+        
+
+    [<JavaScript>]
+    let WarningPanel label =
+        Formlet.Do {
+            let! _ =
+                Formlet.OfElement <| fun _ ->
+                    Div [Attr.Class "warningPanel"] -< [Text label]
+            return! Formlet.Never ()
+        }
+
+    [<JavaScript>]
+    let WithLoadingPane (a: Async<'T>) (f: 'T -> Formlet<'U>) : Formlet<'U> =
+        let loadingPane =
+            Formlet.BuildFormlet <| fun _ ->
+                let elem = 
+                    Div [Attr.Class "loadingPane"]
+                let state = new Event<Result<'T>>()
+                async {
+                    let! x = a
+                    do state.Trigger (Result.Success x)
+                    return ()
+                }
+                |> Async.Start
+                elem, ignore, state.Publish
+        Formlet.Replace loadingPane f
+    
+    [<Inline "window.location = $url">]
+    let Redirect (url: string) = ()
+
+    [<Rpc>]
+    let Login (loginInfo: LoginInfo) =
+        System.Threading.Thread.Sleep 1000
+        if DB.checkLogin loginInfo.Name loginInfo.Password then
+            UserSession.LoginUser loginInfo.Name
+            true
+        else
+            false
+        |> async.Return
+
+    [<JavaScript>]
+    let LoginForm (redirectUrl: string) : Formlet<unit> =
+        let uName =
+            Controls.Input ""
+            |> Validator.IsNotEmpty "Enter Username"
+            |> Enhance.WithValidationIcon
+            |> Enhance.WithTextLabel "Username"
+        let pw =
+            Controls.Password ""
+            |> Validator.IsNotEmpty "Enter Password"
+            |> Enhance.WithValidationIcon
+            |> Enhance.WithTextLabel "Password"
+        let loginF =
+            Formlet.Yield (fun n pw -> {Name=n; Password=pw})
+            <*> uName <*> pw
+
+        Formlet.Do {
+            let! uInfo = 
+                loginF
+                |> Enhance.WithCustomSubmitAndResetButtons
+                    {Enhance.FormButtonConfiguration.Default with Label = Some "Login"}
+                    {Enhance.FormButtonConfiguration.Default with Label = Some "Reset"}
+            return!
+                WithLoadingPane (Login uInfo) <| fun loggedIn ->
+                    if loggedIn then
+                        Redirect redirectUrl
+                        Formlet.Return ()
+                    else
+                        WarningPanel "Login failed"
+        }
+        |> Enhance.WithFormContainer
+
+    type RegisterResult = Success | NotConfirmed | UserExists
+
+    [<Rpc>]
+    let Register (user,pass,confirm) =
+        if pass <> confirm then NotConfirmed
+        else
+            if DB.existsUser user then UserExists
+            else
+                ignore <| DB.addUser user pass
+                Success
+        |> async.Return
+
+    [<JavaScript>]
+    let RegisterForm () : Formlet<unit> =
+        let uName =
+            Controls.Input ""
+            |> Validator.IsNotEmpty "Enter Username"
+            |> Enhance.WithValidationIcon
+            |> Enhance.WithTextLabel "Username"
+        let pw =
+            Controls.Password ""
+            |> Validator.IsNotEmpty "Enter Password"
+            |> Enhance.WithValidationIcon
+            |> Enhance.WithTextLabel "Password"
+        let registerF =
+            Formlet.Yield (fun n pw1 pw2 -> (n,pw1,pw2))
+            <*> uName <*> pw <*> pw
+
+        Formlet.Do {
+            let! uInfo = 
+                registerF
+                |> Enhance.WithCustomSubmitAndResetButtons
+                    {Enhance.FormButtonConfiguration.Default with Label = Some "Login"}
+                    {Enhance.FormButtonConfiguration.Default with Label = Some "Reset"}
+            return!
+                WithLoadingPane (Register uInfo) <| function
+                    | Success ->
+                        Redirect "/"
+                        Formlet.Return ()
+                    | UserExists ->
+                        WarningPanel "User with this name already exists"
+                    | NotConfirmed ->
+                        WarningPanel "Password is not confirmed"
+        }
+        |> Enhance.WithFormContainer
+
+/// Add new movie.
+type AddMovieControl() =
+    inherit IntelliFactory.WebSharper.Web.Control ()
+
+    [<JavaScript>]
+    override this.Body = Forms.AddMovie () :> _
+
+/// Exposes the signup form so that it can be used in sitelets.
+type LoginControl(redirectUrl: string) =
+    inherit IntelliFactory.WebSharper.Web.Control ()
+
+    new () = new LoginControl("?")
+    [<JavaScript>]
+    override this.Body = Forms.LoginForm redirectUrl :> _
+
+/// Add new movie.
+type RegisterControl() =
+    inherit IntelliFactory.WebSharper.Web.Control ()
+
+    [<JavaScript>]
+    override this.Body = Forms.RegisterForm () :> _
+
