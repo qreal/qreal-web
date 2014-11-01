651c1e459c15409c5de92089fae7dc6e68981488
vladimir-zakharov <vladimir.zakharov93@gmail.com>
2014-11-01 19:46:58 +0300

    added log4j config file

--- Robots_diagram/src/main/java/com/qreal/robots/controller/DiagramController.java
+++ Robots_diagram/src/main/java/com/qreal/robots/controller/DiagramController.java
@@ -1,5 +1,6 @@
 package com.qreal.robots.controller;
 
+import org.apache.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,12 +11,15 @@ import org.springframework.web.bind.annotation.RequestMethod;
  */
 @Controller
 public class DiagramController {
+    private static final Logger log = Logger.getLogger(DiagramController.class);
+
     @RequestMapping("/")
     public String home() {
+        log.info("redirect from / to /diagram");
         return "redirect:/diagram";
     }
 
-    @RequestMapping(value="/diagram", method=RequestMethod.GET)
+    @RequestMapping(value = "/diagram", method = RequestMethod.GET)
     public String index(Model model) {
         return "index";
     }
