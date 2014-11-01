e5c94dbebd0676975140ce550723ceac211befd0
vladimir-zakharov <vladimir.zakharov93@gmail.com>
2014-11-01 19:36:39 +0300

    fixed vendor paths

--- Robots_diagram/src/main/webapp/app/vendor.d.ts
+++ Robots_diagram/src/main/webapp/app/vendor.d.ts
@@ -1,7 +1,7 @@
-/// <reference path="../vendor/types/jquery/jquery.d.ts" />
-/// <reference path="../vendor/types/angularjs/angular.d.ts" />
-/// <reference path="../vendor/types/backbone/backbone.d.ts" />
-/// <reference path="../vendor/types/jointjs/jointjs.d.ts" />
+/// <reference path="../resources/types/jquery/jquery.d.ts" />
+/// <reference path="../resources/types/angularjs/angular.d.ts" />
+/// <reference path="../resources/types/backbone/backbone.d.ts" />
+/// <reference path="../resources/types/jointjs/jointjs.d.ts" />
 
 
 
