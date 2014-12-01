3a0d16f3c9f85dc1e7bee1739aff465976d66a09
vladimir-zakharov <vladimir.zakharov93@gmail.com>
2014-12-01 16:26:58 +0300

    idents fix

--- Robots_diagram/setup.sql
+++ Robots_diagram/setup.sql
@@ -11,44 +11,44 @@ CREATE TABLE diagrams (
 
 DROP TABLE IF EXISTS nodes;
 CREATE TABLE nodes (
-	node_id BIGINT NOT NULL AUTO_INCREMENT,
-	name VARCHAR(50) NOT NULL,
-	x DOUBLE NOT NULL,
-	y DOUBLE NOT NULL,
-	image VARCHAR(255) NOT NULL,
-	diagram_id BIGINT,
-	PRIMARY KEY (node_id),
-	FOREIGN KEY (diagram_id) REFERENCES diagrams(diagram_id)
+  node_id    BIGINT       NOT NULL AUTO_INCREMENT,
+  name       VARCHAR(50)  NOT NULL,
+  x          DOUBLE       NOT NULL,
+  y          DOUBLE       NOT NULL,
+  image      VARCHAR(255) NOT NULL,
+  diagram_id BIGINT,
+  PRIMARY KEY (node_id),
+  FOREIGN KEY (diagram_id) REFERENCES diagrams (diagram_id)
 );
 
 DROP TABLE IF EXISTS links;
 CREATE TABLE links (
-	link_id BIGINT NOT NULL AUTO_INCREMENT,
-	source VARCHAR(50) NOT NULL,
-	target VARCHAR(50) NOT NULL,
-	diagram_id BIGINT,
-	PRIMARY KEY (link_id),
-	FOREIGN KEY (diagram_id) REFERENCES diagrams(diagram_id)
+  link_id    BIGINT      NOT NULL AUTO_INCREMENT,
+  source     VARCHAR(50) NOT NULL,
+  target     VARCHAR(50) NOT NULL,
+  diagram_id BIGINT,
+  PRIMARY KEY (link_id),
+  FOREIGN KEY (diagram_id) REFERENCES diagrams (diagram_id)
 );
 
 DROP TABLE IF EXISTS properties;
 CREATE TABLE properties (
-	property_id BIGINT NOT NULL AUTO_INCREMENT,
-	name VARCHAR(50) NOT NULL,
-	value VARCHAR(50) NOT NULL,
-  position INT NOT NULL,
-	node_id BIGINT,
-	PRIMARY KEY (property_id),
-	FOREIGN KEY (node_id) REFERENCES nodes(node_id)
+  property_id BIGINT      NOT NULL AUTO_INCREMENT,
+  name        VARCHAR(50) NOT NULL,
+  value       VARCHAR(50) NOT NULL,
+  position    INT         NOT NULL,
+  node_id     BIGINT,
+  PRIMARY KEY (property_id),
+  FOREIGN KEY (node_id) REFERENCES nodes (node_id)
 );
 
 DROP TABLE IF EXISTS vertices;
 CREATE TABLE vertices (
   vertex_id BIGINT NOT NULL AUTO_INCREMENT,
-  x DOUBLE NOT NULL,
-  y DOUBLE NOT NULL,
-  number INT NOT NULL,
-  link_id BIGINT,
-	PRIMARY KEY (vertex_id),
-	FOREIGN KEY (link_id) REFERENCES links(link_id)
+  x         DOUBLE NOT NULL,
+  y         DOUBLE NOT NULL,
+  number    INT    NOT NULL,
+  link_id   BIGINT,
+  PRIMARY KEY (vertex_id),
+  FOREIGN KEY (link_id) REFERENCES links (link_id)
 );
