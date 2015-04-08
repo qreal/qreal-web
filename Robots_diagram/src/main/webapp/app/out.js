d7cd2b685a43ad2a1c3b50847717b6466ea9bbb3
vladimir-zakharov <vladimir.zakharov93@gmail.com>
2015-04-08 21:36:46 +0300

    robot item refactoring

--- Robots_diagram/src/main/webapp/app/out.js
+++ Robots_diagram/src/main/webapp/app/out.js
@@ -1253,6 +1253,8 @@ var PencilItemImpl = (function () {
 })();
 var RobotItemImpl = (function () {
     function RobotItemImpl(worldModel, position, imageFileName, robot) {
+        this.startCenter = new TwoDPosition();
+        this.center = new TwoDPosition();
         this.width = 50;
         this.height = 50;
         this.sensors = {};
@@ -1261,10 +1263,10 @@ var RobotItemImpl = (function () {
         this.startPosition = position;
         var paper = worldModel.getPaper();
         this.image = paper.image(imageFileName, position.x, position.y, this.width, this.height);
-        this.centerX = position.x + this.width / 2;
-        this.centerY = position.y + this.height / 2;
-        var startCx = this.centerX;
-        var startCy = this.centerY;
+        this.center.x = position.x + this.width / 2;
+        this.center.y = position.y + this.height / 2;
+        this.startCenter.x = this.center.x;
+        this.startCenter.y = this.center.y;
         var handleRadius = 10;
         var handleAttrs = {
             fill: "transparent",
@@ -1287,18 +1289,18 @@ var RobotItemImpl = (function () {
             if (!worldModel.getDrawMode()) {
                 var newX = this.cx + dx;
                 var newY = this.cy + dy;
-                var offsetX = newX - robotItem.centerX;
-                var offsetY = newY - robotItem.centerY;
+                var offsetX = newX - robotItem.center.x;
+                var offsetY = newY - robotItem.center.y;
                 var tan = offsetY / offsetX;
                 var angle = Math.atan(tan) / (Math.PI / 180);
                 if (offsetX < 0) {
                     angle += 180;
                 }
                 angle -= this.rotation;
-                robotItem.image.transform(this.transformation + "R" + angle + "," + robotItem.centerX + "," + robotItem.centerY);
-                robotItem.transformSensorsItems("R" + angle + "," + robotItem.centerX + "," + robotItem.centerY);
-                var newCx = robotItem.image.matrix.x(startCx + robotItem.width / 2 + 20, startCy);
-                var newCy = robotItem.image.matrix.y(startCx + robotItem.width / 2 + 20, startCy);
+                robotItem.image.transform(this.transformation + "R" + angle + "," + robotItem.center.x + "," + robotItem.center.y);
+                robotItem.transformSensorsItems("R" + angle + "," + robotItem.center.x + "," + robotItem.center.y);
+                var newCx = robotItem.image.matrix.x(robotItem.startCenter.x + robotItem.width / 2 + 20, robotItem.startCenter.y);
+                var newCy = robotItem.image.matrix.y(robotItem.startCenter.x + robotItem.width / 2 + 20, robotItem.startCenter.y);
                 this.attr({ cx: newCx, cy: newCy });
             }
             return this;
@@ -1327,8 +1329,8 @@ var RobotItemImpl = (function () {
             return this;
         }, up = function () {
             if (!worldModel.getDrawMode()) {
-                robotItem.centerX = this.matrix.x(startCx, startCy);
-                robotItem.centerY = this.matrix.y(startCx, startCy);
+                robotItem.center.x = this.matrix.x(robotItem.startCenter.x, robotItem.startCenter.y);
+                robotItem.center.y = this.matrix.y(robotItem.startCenter.x, robotItem.startCenter.y);
                 robotItem.updateSensorsTransformations();
             }
             return this;
@@ -1336,6 +1338,16 @@ var RobotItemImpl = (function () {
         this.image.drag(move, start, up);
         this.hideHandles();
     }
+    RobotItemImpl.prototype.setStartPosition = function (position, direction) {
+        this.startPosition = position;
+        this.image.attr({ x: position.x, y: position.y });
+        this.center.x = position.x + this.width / 2;
+        this.center.y = position.y + this.height / 2;
+        this.startCenter.x = this.center.x;
+        this.startCenter.y = this.center.y;
+        this.image.transform("R" + direction + "," + this.center.x + "," + this.center.y);
+        this.rotateHandle.attr({ "cx": +position.x + this.width + 20, "cy": position.y + this.height / 2 });
+    };
     RobotItemImpl.prototype.ride = function () {
         console.log("robot ride");
     };
--- Robots_diagram/src/main/webapp/app/twoDModel/interfaces/engine/items/RobotItem.ts
+++ Robots_diagram/src/main/webapp/app/twoDModel/interfaces/engine/items/RobotItem.ts
@@ -4,4 +4,5 @@ interface RobotItem {
     showHandles(): void;
     removeSensorItem(portName: string): void;
     addSonarSensorItem(portName: string): void;
+    setStartPosition(position: TwoDPosition, direction: number): void;
 }
