fd78155b7a25cf75b814489e7e546b511591ad78
vladimir-zakharov <vladimir.zakharov93@gmail.com>
2015-05-03 19:28:25 +0300

    removed unused methods

--- Robots_diagram/src/main/webapp/app/twoDModel/interfaces/engine/items/WallItem.ts
+++ Robots_diagram/src/main/webapp/app/twoDModel/interfaces/engine/items/WallItem.ts
@@ -2,7 +2,5 @@ interface WallItem extends AbstractItem {
     getPath(): RaphaelPath;
     updateStart(x: number, y: number): void;
     updateEnd(x: number, y: number): void;
-    hideHandles(): void;
-    showHandles(): void;
     remove(): void;
 }
