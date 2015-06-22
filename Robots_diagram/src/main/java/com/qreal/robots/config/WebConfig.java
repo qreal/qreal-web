188e3599a3fbd0e63a0229cc9d256acb45d5cd14
vladimir-zakharov <vladimir.zakharov93@gmail.com>
2015-06-22 16:42:04 +0300

    changed database to h2 in memory

--- Robots_diagram/src/main/java/com/qreal/robots/config/WebConfig.java
+++ Robots_diagram/src/main/java/com/qreal/robots/config/WebConfig.java
@@ -3,13 +3,13 @@ package com.qreal.robots.config;
 
 import com.qreal.robots.dao.DiagramDAO;
 import com.qreal.robots.dao.UserDAO;
-import org.apache.commons.dbcp.BasicDataSource;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import org.springframework.jdbc.datasource.SimpleDriverDataSource;
 import org.springframework.orm.hibernate4.HibernateTransactionManager;
 import org.springframework.orm.hibernate4.LocalSessionFactoryBuilder;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
@@ -19,7 +19,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
 
-import java.sql.SQLException;
+import javax.sql.DataSource;
 import java.util.Properties;
 
 @Configuration
@@ -51,15 +51,22 @@ public class WebConfig extends WebMvcConfigurerAdapter {
     }
 
     @Bean(name = "dataSource")
-    public BasicDataSource dataSource() {
-        BasicDataSource dataSource = new BasicDataSource();
+    public DataSource dataSource() {
+        /*BasicDataSource dataSource = new BasicDataSource();
         dataSource.setDriverClassName("com.mysql.jdbc.Driver");
-        dataSource.setUrl("jdbc:mysql://localhost:3306/diagram");
+        dataSource.setUrl("jdbc:mysql://localhost:3306/diagram;");
         dataSource.setUsername("user");
         dataSource.setPassword("user");
         dataSource.setValidationQuery("SELECT 1");
         dataSource.setTestOnBorrow(true);
 
+        return dataSource;*/
+
+        SimpleDriverDataSource dataSource = new SimpleDriverDataSource();
+        dataSource.setDriverClass(org.h2.Driver.class);
+        dataSource.setUsername("sa");
+        dataSource.setUrl("jdbc:h2:mem:test;DB_CLOSE_DELAY=-1;INIT=CREATE SCHEMA IF NOT EXISTS diagram");
+        dataSource.setPassword("");
         return dataSource;
     }
 
@@ -94,6 +101,8 @@ public class WebConfig extends WebMvcConfigurerAdapter {
     private Properties getHibernateProperties() {
         Properties properties = new Properties();
         properties.put("hibernate.dialect", "org.hibernate.dialect.MySQLDialect");
+        properties.put("hibernate.hbm2ddl.auto", "create-drop");
+        properties.put("hibernate.hbm2ddl.import_files", "data.sql");
         return properties;
     }
 
---
+++ Robots_diagram/src/main/resources/data.sql
@@ -0,0 +1,2 @@
+INSERT INTO users (username, password, enabled) VALUES ('user', '$2a$04$eWY0Czs5avEcJTDVRcB66.8n5mQmMnRCCTi6P/9oNAncfzyb8.D7e', TRUE);
+INSERT INTO user_roles (username, ROLE) VALUES ('user', 'ROLE_USER');
