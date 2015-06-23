974e750b5edfe5f670a9b94cf84da221123150f8
vladimir-zakharov <vladimir.zakharov93@gmail.com>
2015-06-23 16:41:03 +0300

    added custom 404 error page

--- Robots_diagram/src/main/java/com/qreal/robots/config/core/AppInit.java
+++ Robots_diagram/src/main/java/com/qreal/robots/config/core/AppInit.java
@@ -3,6 +3,9 @@ package com.qreal.robots.config.core;
 import com.qreal.robots.config.WebConfig;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
+import javax.servlet.ServletContext;
+import javax.servlet.ServletRegistration;
+
 public class AppInit extends AbstractAnnotationConfigDispatcherServletInitializer {
 
     @Override
@@ -23,6 +26,11 @@ public class AppInit extends AbstractAnnotationConfigDispatcherServletInitialize
     @Override
     protected String[] getServletMappings() {
         return new String[]{"/"};
+    }
+
+    @Override
+    protected void customizeRegistration(ServletRegistration.Dynamic registration) {
+        registration.setInitParameter("throwExceptionIfNoHandlerFound", "true");
     }
 
 }
