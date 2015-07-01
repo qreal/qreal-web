6d9394bea17792d28cb3d3dcd3e8476cc2612a69
vladimir-zakharov <vladimir.zakharov93@gmail.com>
2015-07-01 16:37:27 +0300

    added images cache period

--- Robots_diagram/src/main/java/com/qreal/robots/config/WebConfig.java
+++ Robots_diagram/src/main/java/com/qreal/robots/config/WebConfig.java
@@ -28,13 +28,16 @@ import java.util.Properties;
 @ComponentScan("com.qreal.robots")
 public class WebConfig extends WebMvcConfigurerAdapter {
 
+    /* Time, in seconds, to have the browser cache static resources (one week). */
+    private static final int BROWSER_CACHE_CONTROL = 604800;
+
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/WEB-INF/pages/**").addResourceLocations("/pages/");
         registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
         registry.addResourceHandler("/app/**").addResourceLocations("/app/");
         registry.addResourceHandler("/configs/**").addResourceLocations("/configs/");
-        registry.addResourceHandler("/images/**").addResourceLocations("/images/");
+        registry.addResourceHandler("/images/**").addResourceLocations("/images/").setCachePeriod(BROWSER_CACHE_CONTROL);
 
     }
 
