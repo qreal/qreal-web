3d8fde1d4c8b1395e912283337cb371757361260
vladimir-zakharov <vladimir.zakharov93@gmail.com>
2015-08-03 18:11:08 +0300

    updated typescript, dependencies

--- StepicRobotsWeb/src/main/webapp/resources/types/jointjs/jointjs.d.ts
+++ StepicRobotsWeb/src/main/webapp/resources/types/jointjs/jointjs.d.ts
@@ -1,6 +1,6 @@
-// Type definitions for Joint JS 0.6
+// Type definitions for Joint JS 0.9.3
 // Project: http://www.jointjs.com/
-// Definitions by: Aidan Reel <http://github.com/areel>, David Durman <http://github.com/DavidDurman>
+// Definitions by: Aidan Reel <http://github.com/areel>, David Durman <http://github.com/DavidDurman>, Ewout Van Gossum <https://github.com/DenEwout>
 // Definitions: https://github.com/borisyankov/DefinitelyTyped
 
 /// <reference path="../backbone/backbone.d.ts" />
@@ -16,68 +16,52 @@ declare module joint {
         }
 
         class Graph extends Backbone.Model {
-            initialize();
-
-            fromJSON(json:any);
-
-            clear();
-
-            addCell(cell:Cell);
-
-            addCells(cells:Cell[]);
-
+            addCell(cell:Cell) : void;
+            addCells(cells:Cell[]) : void;
+            initialize() : void;
+            fromJSON(json:any) : void;
+            toJSON() : Object;
+            clear() : void;
             getConnectedLinks(cell:Cell, opt?:any):Link[];
-
-            disconnectLinks(cell:Cell);
-
-            removeLinks(cell:Cell);
+            disconnectLinks(cell:Cell) : void;
+            removeLinks(cell:Cell) : void;
+            findModelsFromPoint(point:{x : number; y: number}):Element[];
 
             getLinks();
-
             getElements();
-
             getCell(id);
         }
 
         class Cell extends Backbone.Model {
-            toJSON();
-
-            remove(options?:any);
-
-            toFront();
-
-            toBack();
-
-            embed(cell:Cell);
-
-            unembed(cell:Cell);
-
+            toJSON() : Object;
+            remove(options?:any) : void;
+            toFront() : void;
+            toBack() : void;
+            embed(cell:Cell) : void;
+            unembed(cell:Cell) : void;
             getEmbeddedCells():Cell[];
-
             clone(opt?:any):Backbone.Model;      // @todo: return can either be Cell or Cell[].
             attr(attrs:any):Cell;
+            diagramElementView: ElementView;
         }
-
 
         class Element extends Cell {
             position(x:number, y:number):Element;
-
             translate(tx:number, ty?:number):Element;
-
             resize(width:number, height:number):Element;
-
-            rotate(angle:number, absolute):Element;
+            rotate(angle:number, options : {absolute : boolean; origin: {x:number;y:number}}):Element;
+            remove(): void;
         }
+
         interface IDefaults {
             type: string;
         }
 
         class Link extends Cell {
-            defaults:IDefaults;
-
+            defaults():IDefaults;
             disconnect():Link;
-
             label(idx?:number, value?:any):any;   // @todo: returns either a label under idx or Link if both idx and value were passed
+            remove(): void;
         }
 
         interface IOptions {
@@ -86,65 +70,54 @@ declare module joint {
             gridSize: number;
             perpendicularLinks: boolean;
             elementView: ElementView;
-            diagramElementView: ElementView;
             linkView: LinkView;
         }
 
-        class Paper extends Backbone.View {
+        class Paper extends Backbone.View<Backbone.Model> {
             options:IOptions;
 
-            setDimensions(width:number, height:number);
-
+            setDimensions(width:number, height:number) : void;
             scale(sx:number, sy?:number, ox?:number, oy?:number):Paper;
-
             rotate(deg:number, ox?:number, oy?:number):Paper;      // @todo not released yet though it's in the source code already
             findView(el:any):CellView;
-
             findViewByModel(modelOrId:any):CellView;
-
-            findViewsFromPoint(p:{ x: number; y: number;
-            }):CellView[];
-
-            findViewsInArea(r:{ x: number; y: number; width: number; height: number;
-            }):CellView[];
-
-            snapToGrid(p):{ x: number; y: number;
-            };
+            findViewsFromPoint(p:{ x: number; y: number; }):CellView[];
+            findViewsInArea(r:{ x: number; y: number; width: number; height: number; }):CellView[];
+            fitToContent(opt?:any): void;
         }
 
         class ElementView extends CellView {
-            scale(sx:number, sy:number);
+            scale(sx:number, sy:number) : void;
         }
-        class CellView extends Backbone.View {
+
+        class CellView extends Backbone.View<Cell> {
             paper;
             _linkView;
             _dx;
             _dy;
 
-            getBBox():{ x: number; y: number; width: number; height: number;
-            };
+            getBBox():{ x: number; y: number; width: number; height: number; };
+            highlight(el?:any): void;
+            unhighlight(el?:any): void;
+            findMagnet(el:any): void;
+            getSelector(el:any): void;
 
-            highlight(el?:any);
-
-            unhighlight(el?:any);
-
-            findMagnet(el:any);
-
-            getSelector(el:any);
-
-            pointerdown(evt, x, y);
+            pointerdblclick(evt:any, x:number, y:number):void;
+            pointerclick(evt:any, x:number, y:number):void;
+            pointerdown(evt:any, x:number, y:number):void;
+            pointermove(evt:any, x:number, y:number):void;
+            pointerup(evt:any, x:number, y:number):void;
         }
 
         class LinkView extends CellView {
             getConnectionLength():number;
-
-            getPointAtLength(length:number):{ x: number; y: number;
-            };
+            getPointAtLength(length:number):{ x: number; y: number; };
         }
 
     }
-    module ui {
-    }
+
+    module ui {}
+
     module shapes {
         module basic {
             class Generic extends joint.dia.Element {
@@ -158,12 +131,10 @@ declare module joint {
             class Image extends Generic {
             }
 
-            class Path extends Generic {
-            }
-
             class PortsViewInterface {
             }
         }
+
 
         module devs {
             class Generic extends joint.dia.Element {
@@ -184,19 +155,15 @@ declare module joint {
             class ImageWithPorts extends Generic {
             }
         }
-
     }
+
     module util {
         function uuid():string;
-
         function guid(obj:any):string;
-
         function mixin(objects:any[]):any;
-
         function supplement(objects:any[]):any;
-
         function deepMixin(objects:any[]):any;
-
         function deepSupplement(objects:any[], defaultIndicator?:any):any;
     }
+
 }
